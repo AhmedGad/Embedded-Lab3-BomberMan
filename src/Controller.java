c077f5d0a0dac2048683726030f551bafafb2686
Ahmed <ahmed@Ahmed.PC>
2013-03-01 19:39:17 +0200

    ..

---
+++ src/Controller.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author Ahmed
+ */
+public class Controller extends MIDlet {
+
+	public void startApp() {
+		System.out.println("YES :P");
+	}
+
+	public void pauseApp() {
+	}
+
+	public void destroyApp(boolean unconditional) {
+	}
+}
