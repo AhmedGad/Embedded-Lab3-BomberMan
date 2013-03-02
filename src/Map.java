7c10bc03ea6a7d8251c7620397ed3ab38340d6a5
Gad <Ahmed.Mu.Gad@gmail.com>
2013-03-02 02:13:50 +0200

    beta

--- src/Map.java
+++ src/Map.java
@@ -1,60 +1,61 @@
+
 import java.util.Random;
 import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.game.TiledLayer;
 
-
 public class Map extends TiledLayer {
-	public static final int breakable = 1;
-	public static final int nonbreak = 2;
-	private static Random r = new Random();
-	private int[][] map;
-	private int maxMobs; // maximum number of monsters for this level
 
-	public Map(Image tiles, int width, int height, int level) {
-		super(width, height, tiles,
-				tiles.getWidth()/2, tiles.getHeight());
-		map = new int[width][height];
-		// make number of mobs depend on the size too?!
-		maxMobs = level * 4;
+    public static final int breakable = 1;
+    public static final int nonbreak = 2;
+    private static Random r = new Random();
+    private int[][] map;
+    private int maxMobs; // maximum number of monsters for this level
 
-		// generating map
-		fillMap();
+    public Map(Image tiles, int width, int height, int level) {
+        super(width, height, tiles,
+                tiles.getWidth() / 2, tiles.getHeight());
+        map = new int[width][height];
+        // make number of mobs depend on the size too?!
+        maxMobs = level * 4;
 
-		// filling the TiledLayer
-		for (int i = 0; i < map.length; i++)
-			for (int j = 0; j < map[0].length; j++)
-				setCell(i, j, map[i][j]);
-	}
+        // generating map
+        fillMap();
 
-	private void fillMap() {
-		for (int i = 0; i < map.length; i++) {
-//			System.out.println(i +" i");
-			for (int j = 0; j < map[0].length; j++) {
-//				System.out.println(j+ " j");
-				if (i == 0 && j == 0) {
-					// starting cell
-					map[i][j] = 0;
-					continue;
-				}
-				if (i % 2 == 1 && j % 2 == 1) {
-					map[i][j] = nonbreak;
-				} else {
-					if (r.nextInt() > 1000000000 &&(i> 4 && j > 4)) {
-						map[i][j] = breakable;
-					} else {
-						map[i][j] = 0;
-					}
-				}
-			}
-		}
-	}
+        // filling the TiledLayer
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                setCell(i, j, map[i][j]);
+            }
+        }
+    }
 
-	public boolean clearCell(int i, int j) {
-		if (map[i][j] == breakable) {
-			map[i][j] = 0;
-			return true;
-		} else {
-			return false;
-		}
-	}
+    private void fillMap() {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (i == 0 && j == 0) {
+                    // starting cell
+                    map[i][j] = 0;
+                    continue;
+                }
+                if (i % 2 == 1 && j % 2 == 1) {
+                    map[i][j] = nonbreak;
+                } else {
+                    if (r.nextInt() > 1000000000) {
+                        map[i][j] = breakable;
+                    } else {
+                        map[i][j] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean clearCell(int i, int j) {
+        if (map[i][j] == breakable) {
+            map[i][j] = 0;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
